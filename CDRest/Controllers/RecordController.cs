5b98e374d0836988f6370996fcf17c10fc6000c9
Jonas Grønquist <jog002@edu.zealand.dk>
2024-11-18 12:26:10 +0100

    updated

---
+++ CDRest/Controllers/RecordController.cs
@@ -0,0 +1,64 @@
+﻿using CDRest.Model;
+using CDRest.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Xml.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CDRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordController : ControllerBase
+    {
+        private readonly RecordRepo _repository;
+        public RecordController(RecordRepo repository)
+        {
+            _repository = repository;
+        }
+        // GET: api/<RecordCtrlr>
+        [HttpGet]
+        public ActionResult <IEnumerable<Record>> Get()
+        {
+            try
+            {
+                var records = _repository.GetAll();
+                if (records == null || !records.Any())
+                {
+                    return NotFound("No records found");
+                }
+                return Ok(records);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
+        // GET api/<RecordCtrlr>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RecordCtrlr>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RecordCtrlr>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RecordCtrlr>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CDRest/Repositories/RecordRepo.cs
@@ -0,0 +1,21 @@
+﻿using CDRest.Model;
+
+namespace CDRest.Repositories
+{
+    public class RecordRepo
+    {
+        private static int _nextId = 1;
+        private static readonly List<Record> Data = new()
+        {
+            new Record {Id = _nextId++, Title = "sang1", Artist = "Artist1", Year = 2008, Duration = 212},
+            new Record {Id = _nextId++, Title = "sang2", Artist = "Artist2", Year = 2012, Duration = 223}
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
+        };
+
+        public List <Record> GetAll()
+        {
+            List<Record> records = new(Data);
+            return records;
+        }
+    }
+}
