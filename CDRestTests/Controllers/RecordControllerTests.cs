3bc390979b652001b02a90934c249944fafdefbb
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-11-18 12:33:48 +0100

    Test project added;

---
+++ CDRestTests/Controllers/RecordControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CDRest.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDRest.Controllers.Tests
+{
+    [TestClass()]
+    public class RecordControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
