ffb4125d956627bab81d946e348b23b367d11883
Jonas Grønquist <jog002@edu.zealand.dk>
2024-11-18 12:59:21 +0100

    test

--- CDRestTests/Controllers/RecordControllerTests.cs
+++ CDRestTests/Controllers/RecordControllerTests.cs
@@ -5,16 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CDRest.Repositories;
 
 namespace CDRest.Controllers.Tests
 {
     [TestClass()]
     public class RecordControllerTests
     {
+        RecordController controller;
+        RecordRepo repo;
+        [TestInitialize]
+        public void Setup()
+        {
+            repo = new RecordRepo();
+            controller = new RecordController(repo);
+        }
         [TestMethod()]
         public void GetTest()
         {
-            Assert.Fail();
+            Assert.AreEqual(2, controller.Get().Value.Count());
         }
     }
 }
